5f57d9d016b30b5466d454b9bd80c3069a710fd5
Filipe Ramos <filipe.ramos92@hotmail.com>
2021-11-17 10:40:01 -0300

    Operadores e funções

---
+++ funcoes.js
@@ -0,0 +1,15 @@
+
+// Declarando Função
+function imprimeTexto(texto)  {
+    console.log(texto)
+}
+
+imprimeTexto('oi')
+
+function soma() {
+    return 2 + 2;
+}
+
+console.log(soma())
+
+imprimeTexto(soma())
---
+++ template-string.js
@@ -0,0 +1,8 @@
+const nome = 'Filipe';
+const idade = 2021 - 1992;
+const cidadeNascimento = 'Niterói';
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e 
+nasci na cidade de ${cidadeNascimento}`;
+
+console.log(apresentacao)
